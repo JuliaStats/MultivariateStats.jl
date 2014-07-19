445e7af3ce7ce9927e8ef2c8c8fbb1a4af2bb36b
Dahua Lin <lindahua@gmail.com>
2014-07-19 11:16:17 -0500

    implement ridge regression

--- src/lreg.jl
+++ src/lreg.jl
@@ -1,6 +1,6 @@
 # Ridge Regression (Tikhonov regularization)
 
-## auxiliary
+#### auxiliary
 
 function lreg_chkdims(X::AbstractMatrix, Y::AbstractVecOrMat, trans::Bool)
     mX, nX = size(X)
@@ -16,6 +16,7 @@ lrsoltype{T}(::DenseMatrix{T}) = Matrix{T}
 _vaug{T}(X::DenseMatrix{T}) = vcat(X, ones(T, 1, size(X,2)))::Matrix{T}
 _haug{T}(X::DenseMatrix{T}) = hcat(X, ones(T, size(X,1), 1))::Matrix{T}
 
+
 ## linear least square
 
 function llsq{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T}; 
@@ -29,52 +30,79 @@ function llsq{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T};
         size(Y, 1) == mX || throw(DimensionMismatch("Dimensions of X and Y mismatch."))
         mX >= nX || error("mX >= nX is required when trans is false.")
     end
-    _llsq(X, Y, trans, bias)
+    _ridge(X, Y, zero(T), trans, bias)
 end
-
-function _llsq{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T}, trans::Bool, bias::Bool)
-    if bias
-        if trans
-            X_ = _vaug(X)
-            A = cholfact!(A_mul_Bt(X_, X_)) \ (X_ * Y)
-        else
-            X_ = _haug(X)
-            A = cholfact!(X_'X_) \ (X_'Y)
-        end
-    else
-        if trans
-            A = cholfact!(A_mul_Bt(X, X)) \ (X * Y)
-        else
-            A = cholfact!(X'X) \ (X'Y)
-        end
-    end
-    return A::lrsoltype(Y)
-end
-
 
 ## ridge regression
 
 function ridge{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T}, r::Real; 
                                 trans::Bool=false, bias::Bool=true)
     lreg_chkdims(X, Y, trans)
+    r >= zero(r) || error("r must be non-negative.")
     _ridge(X, Y, convert(T, r), trans, bias)
 end
 
 function ridge{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T}, r::DenseVector{T}; 
                                 trans::Bool=false, bias::Bool=true)
     d = lreg_chkdims(X, Y, trans)
-    length(r) == d || throw("Incorrect length of r.")
+    length(r) == d || throw(DimensionMismatch("Incorrect length of r."))
     _ridge(X, Y, r, trans, bias)
 end
 
 function ridge{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T}, r::DenseMatrix{T}; 
                                 trans::Bool=false, bias::Bool=true)
     d = lreg_chkdims(X, Y, trans)
-    size(r) == (d, d) || throw("Incorrect size of r.")
+    size(r) == (d, d) || throw(DimensionMismatch("Incorrect size of r."))
     _ridge(X, Y, r, trans, bias)
 end
 
-function _ridge(X::DenseMatrix, Y::DenseVecOrMat, r::Real, trans::Bool, bias::Bool)
+## implementation
 
+function _ridge{T<:FloatingPoint}(X::DenseMatrix{T}, Y::DenseVecOrMat{T}, 
+                                  r::Union(Real, DenseVecOrMat), trans::Bool, bias::Bool)
+    if bias
+        if trans
+            X_ = _vaug(X)
+            A = cholfact!(_ridge_reg!(A_mul_Bt(X_, X_), r, bias)) \ (X_ * Y)
+        else
+            X_ = _haug(X)
+            A = cholfact!(_ridge_reg!(X_'X_, r, bias)) \ (X_'Y)
+        end
+    else
+        if trans
+            A = cholfact!(_ridge_reg!(A_mul_Bt(X, X), r, bias)) \ (X * Y)
+        else
+            A = cholfact!(_ridge_reg!(X'X, r, bias)) \ (X'Y)
+        end
+    end
+    return A::lrsoltype(Y)
+end
+
+function _ridge_reg!(Q::Matrix, r::Real, bias::Bool)
+    if r > zero(r)
+        n = size(Q, 1) - int(bias)
+        for i = 1:n
+            @inbounds Q[i,i] += r
+        end
+    end
+    return Q
+end
+
+function _ridge_reg!(Q::Matrix, r::DenseVector, bias::Bool)
+    n = size(Q, 1) - int(bias)
+    @assert length(r) == n
+    for i = 1:n
+        @inbounds Q[i,i] += r[i]
+    end
+    return Q
+end
+
+function _ridge_reg!(Q::Matrix, r::Matrix, bias::Bool)
+    n = size(Q, 1) - int(bias)
+    @assert size(r) == (n, n)
+    for j = 1:n, i = 1:n
+        @inbounds Q[i,j] += r[i,j]
+    end
+    return Q
 end
 
--- test/lreg.jl
+++ test/lreg.jl
@@ -45,7 +45,7 @@ Aa = llsq(X, Y1; trans=false, bias=true)
 Aa_r = copy(Aa)
 @test size(Aa) == (n+1, n2)
 A, b = Aa[1:end-1,:], Aa[end,:]
-@test_approx_eq X'Y1 X' * (X * A .+ b)
+@test_approx_eq X' * (Y1 .- b) X'X * A
 
 aa = llsq(X, y1; trans=false, bias=true)
 @test_approx_eq aa Aa[:,1]
@@ -54,5 +54,117 @@ Aa = llsq(Xt, Y1; trans=true, bias=true)
 @test_approx_eq Aa Aa_r
 
 aa = llsq(Xt, y1; trans=true, bias=true)
+@test_approx_eq aa Aa[:,1]
+
+
+## ridge (with Real r)
+
+r = 0.1
+
+A = ridge(X, Y0, r; trans=false, bias=false)
+A_r = copy(A)
+@test size(A) == (n, n2)
+@test_approx_eq X'Y0 (X'X + r * eye(n)) * A
+
+a = ridge(X, y0, r; trans=false, bias=false)
+@test size(a) == (n,)
+@test_approx_eq a A[:,1]
+
+A = ridge(Xt, Y0, r; trans=true, bias=false)
+@test size(A) == (n, n2)
+@test_approx_eq A A_r
+
+a = ridge(Xt, y0, r; trans=true, bias=false)
+@test size(a) == (n,)
+@test_approx_eq a A[:,1]
+
+Aa = ridge(X, Y1, r; trans=false, bias=true)
+Aa_r = copy(Aa)
+@test size(Aa) == (n+1, n2)
+A, b = Aa[1:end-1,:], Aa[end,:]
+@test_approx_eq X' * (Y1 .- b) (X'X + r * eye(n)) * A
+
+aa = ridge(X, y1, r; trans=false, bias=true)
+@test_approx_eq aa Aa[:,1]
+
+Aa = ridge(Xt, Y1, r; trans=true, bias=true)
+@test_approx_eq Aa Aa_r
+
+aa = ridge(Xt, y1, r; trans=true, bias=true)
+@test_approx_eq aa Aa[:,1]
+
+
+## ridge (with diagonal r)
+
+r = 0.05 + 0.1 * rand(n)
+
+A = ridge(X, Y0, r; trans=false, bias=false)
+A_r = copy(A)
+@test size(A) == (n, n2)
+@test_approx_eq X'Y0 (X'X + diagm(r)) * A
+
+a = ridge(X, y0, r; trans=false, bias=false)
+@test size(a) == (n,)
+@test_approx_eq a A[:,1]
+
+A = ridge(Xt, Y0, r; trans=true, bias=false)
+@test size(A) == (n, n2)
+@test_approx_eq A A_r
+
+a = ridge(Xt, y0, r; trans=true, bias=false)
+@test size(a) == (n,)
+@test_approx_eq a A[:,1]
+
+Aa = ridge(X, Y1, r; trans=false, bias=true)
+Aa_r = copy(Aa)
+@test size(Aa) == (n+1, n2)
+A, b = Aa[1:end-1,:], Aa[end,:]
+@test_approx_eq X' * (Y1 .- b) (X'X + diagm(r)) * A
+
+aa = ridge(X, y1, r; trans=false, bias=true)
+@test_approx_eq aa Aa[:,1]
+
+Aa = ridge(Xt, Y1, r; trans=true, bias=true)
+@test_approx_eq Aa Aa_r
+
+aa = ridge(Xt, y1, r; trans=true, bias=true)
+@test_approx_eq aa Aa[:,1]
+
+
+## ridge (with qudratic r matrix)
+
+Q = qr(randn(n, n))[1]
+r = Q' * diagm(r) * Q
+
+A = ridge(X, Y0, r; trans=false, bias=false)
+A_r = copy(A)
+@test size(A) == (n, n2)
+@test_approx_eq X'Y0 (X'X + r) * A
+
+a = ridge(X, y0, r; trans=false, bias=false)
+@test size(a) == (n,)
+@test_approx_eq a A[:,1]
+
+A = ridge(Xt, Y0, r; trans=true, bias=false)
+@test size(A) == (n, n2)
+@test_approx_eq A A_r
+
+a = ridge(Xt, y0, r; trans=true, bias=false)
+@test size(a) == (n,)
+@test_approx_eq a A[:,1]
+
+Aa = ridge(X, Y1, r; trans=false, bias=true)
+Aa_r = copy(Aa)
+@test size(Aa) == (n+1, n2)
+A, b = Aa[1:end-1,:], Aa[end,:]
+@test_approx_eq X' * (Y1 .- b) (X'X + r) * A
+
+aa = ridge(X, y1, r; trans=false, bias=true)
+@test_approx_eq aa Aa[:,1]
+
+Aa = ridge(Xt, Y1, r; trans=true, bias=true)
+@test_approx_eq Aa Aa_r
+
+aa = ridge(Xt, y1, r; trans=true, bias=true)
 @test_approx_eq aa Aa[:,1]
 
