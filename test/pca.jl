4dfcb1c675c42e7225984f80c1c7a04e3be94001
Art <wildart@gmail.com>
2018-08-14 13:48:56 -0400

    fixed pca & ppca

--- test/pca.jl
+++ test/pca.jl
@@ -1,14 +1,17 @@
 using MultivariateStats
-using Base.Test
+using LinearAlgebra
+using Test
+import Statistics: mean, cov
+import Random
 
-srand(34568)
+Random.seed!(34568)
 
 ## PCA with zero mean
 
 X = randn(5, 10)
 Y = randn(3, 10)
 
-P = qr(randn(5, 5))[1][:, 1:3]
+P = qr(randn(5, 5)).Q[:, 1:3]
 pvars = [5., 4., 3.]
 M = PCA(Float64[], P, pvars, 15.0)
 
@@ -32,12 +35,12 @@ M = PCA(Float64[], P, pvars, 15.0)
 
 ## PCA with non-zero mean
 
-mv = rand(5)
-M = PCA(mv, P, pvars, 15.0)
+mval = rand(5)
+M = PCA(mval, P, pvars, 15.0)
 
 @test indim(M) == 5
 @test outdim(M) == 3
-@test mean(M) == mv
+@test mean(M) == mval
 @test projection(M) == P
 @test principalvars(M) == pvars
 @test principalvar(M, 2) == pvars[2]
@@ -46,11 +49,11 @@ M = PCA(mv, P, pvars, 15.0)
 @test tresidualvar(M) == 3.0
 @test principalratio(M) == 0.8
 
-@test transform(M, X[:,1]) ≈ P' * (X[:,1] .- mv)
-@test transform(M, X) ≈ P' * (X .- mv)
+@test transform(M, X[:,1]) ≈ P' * (X[:,1] .- mval)
+@test transform(M, X) ≈ P' * (X .- mval)
 
-@test reconstruct(M, Y[:,1]) ≈ P * Y[:,1] .+ mv
-@test reconstruct(M, Y) ≈ P * Y .+ mv
+@test reconstruct(M, Y[:,1]) ≈ P * Y[:,1] .+ mval
+@test reconstruct(M, Y) ≈ P * Y .+ mval
 
 
 ## prepare training data
@@ -58,15 +61,15 @@ M = PCA(mv, P, pvars, 15.0)
 d = 5
 n = 1000
 
-R = qr(randn(d, d))[1]
-@test R'R ≈ eye(5)
-scale!(R, sqrt.([0.5, 0.3, 0.1, 0.05, 0.05]))
+R = collect(qr(randn(d, d)).Q)
+@test R'R ≈ Matrix(I, 5, 5)
+rmul!(R, Diagonal(sqrt.([0.5, 0.3, 0.1, 0.05, 0.05])))
 
 X = R'randn(5, n) .+ randn(5)
-mv = vec(mean(X, 2))
-Z = X .- mv
+mval = vec(mean(X, dims=2))
+Z = X .- mval
 
-C = cov(X, 2)
+C = cov(X, dims=2)
 pvs0 = sort(eigvals(C); rev=true)
 tv = sum(diag(C))
 
@@ -78,8 +81,8 @@ pvs = principalvars(M)
 
 @test indim(M) == 5
 @test outdim(M) == 5
-@test mean(M) == mv
-@test P'P ≈ eye(5)
+@test mean(M) == mval
+@test P'P ≈ Matrix(I, 5, 5)
 @test C*P ≈ P*Diagonal(pvs)
 @test issorted(pvs; rev=true)
 @test pvs ≈ pvs0
@@ -87,7 +90,7 @@ pvs = principalvars(M)
 @test sum(pvs) ≈ tvar(M)
 @test reconstruct(M, transform(M, X)) ≈ X
 
-M = fit(PCA, X; mean=mv)
+M = fit(PCA, X; mean=mval)
 @test projection(M) ≈ P
 
 M = fit(PCA, Z; mean=0)
@@ -98,14 +101,14 @@ P = projection(M)
 
 @test indim(M) == 5
 @test outdim(M) == 3
-@test P'P ≈ eye(3)
+@test P'P ≈ Matrix(I, 3, 3)
 @test issorted(pvs; rev=true)
 
 M = fit(PCA, X; pratio=0.85)
 
 @test indim(M) == 5
 @test outdim(M) == 3
-@test P'P ≈ eye(3)
+@test P'P ≈ Matrix(I, 3, 3)
 @test issorted(pvs; rev=true)
 
 ## pcastd
@@ -116,8 +119,8 @@ pvs = principalvars(M)
 
 @test indim(M) == 5
 @test outdim(M) == 5
-@test mean(M) == mv
-@test P'P ≈ eye(5)
+@test mean(M) == mval
+@test P'P ≈ Matrix(I, 5, 5)
 @test isapprox(C*P, P*Diagonal(pvs), atol=1.0e-3)
 @test issorted(pvs; rev=true)
 @test isapprox(pvs, pvs0, atol=1.0e-3)
@@ -125,7 +128,7 @@ pvs = principalvars(M)
 @test sum(pvs) ≈ tvar(M)
 @test reconstruct(M, transform(M, X)) ≈ X
 
-M = fit(PCA, X; method=:svd, mean=mv)
+M = fit(PCA, X; method=:svd, mean=mval)
 @test projection(M) ≈ P
 
 M = fit(PCA, Z; method=:svd, mean=0)
@@ -136,14 +139,14 @@ P = projection(M)
 
 @test indim(M) == 5
 @test outdim(M) == 3
-@test P'P ≈ eye(3)
+@test P'P ≈ Matrix(I, 3, 3)
 @test issorted(pvs; rev=true)
 
 M = fit(PCA, X; method=:svd, pratio=0.85)
 
 @test indim(M) == 5
 @test outdim(M) == 3
-@test P'P ≈ eye(3)
+@test P'P ≈ Matrix(I, 3, 3)
 @test issorted(pvs; rev=true)
 
 # test that fit works with Float32 values
