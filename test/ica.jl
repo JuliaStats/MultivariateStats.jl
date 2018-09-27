f745f5b34ce066e88c2b9c9ca13e7f0809c7b7d8
wildart <wildart@gmail.com>
2018-09-27 18:23:58 -0400

    ICA: Fixed convergence criteria (closes #45). Added testsets. Removed `Printf` dependency.

--- test/ica.jl
+++ test/ica.jl
@@ -5,100 +5,116 @@ import Statistics: mean, cov
 import Random
 import StatsBase
 
-Random.seed!(15678)
+@testset "ICA" begin
 
-## icagfun
+    Random.seed!(15678)
 
-f = icagfun(:tanh)
-u, v = evaluate(f, 1.5)
-@test u ≈ 0.905148253644866438242
-@test v ≈ 0.180706638923648530597
+    function generatetestdata(n, k, m)
+        t = range(0.0, step=10.0, length=n)
+        s1 = sin.(t * 2)
+        s2 = s2 = 1.0 .- 2.0 * Bool[isodd(floor(Int, x / 3)) for x in t]
+        s3 = Float64[mod(x, 5.0) for x in t]
 
-f = icagfun(:tanh, Float32)
-u, v = evaluate(f, 1.5f0)
-@test u ≈ 0.90514827f0
-@test v ≈ 0.18070662f0
+        s1 += 0.1 * randn(n)
+        s2 += 0.1 * randn(n)
+        s3 += 0.1 * randn(n)
 
-f = icagfun(:tanh, 1.5)
-u, v = evaluate(f, 1.2)
-@test u ≈ 0.946806012846268289646
-@test v ≈ 0.155337561057228069719
+        S = hcat(s1, s2, s3)'
+        @assert size(S) == (k, n)
 
-f = icagfun(:tanh, 1.5f0)
-u, v = evaluate(f, 1.2f0)
-@test u ≈ 0.94680610f0
-@test v ≈ 0.15533754f0
+        A = randn(m, k)
 
-f = icagfun(:gaus)
-u, v = evaluate(f, 1.5)
-@test u ≈ 0.486978701037524594696
-@test v ≈ -0.405815584197937162246
+        X = A * S
+        mv = vec(mean(X, dims=2))
+        @assert size(X) == (m, n)
+        C = cov(X, dims=2)
+        return X, mv, C
+    end
 
-f = icagfun(:gaus, Float32)
-u, v = evaluate(f, 1.5f0)
-@test u ≈ 0.4869787f0
-@test v ≈ -0.40581557f0
+    @testset "Auxiliary" begin
 
+        f = icagfun(:tanh)
+        u, v = evaluate(f, 1.5)
+        @test u ≈ 0.905148253644866438242
+        @test v ≈ 0.180706638923648530597
 
-## data
+        f = icagfun(:tanh, Float32)
+        u, v = evaluate(f, 1.5f0)
+        @test u ≈ 0.90514827f0
+        @test v ≈ 0.18070662f0
 
-# sources
-n = 1000
-k = 3
-m = 8
+        f = icagfun(:tanh, 1.5)
+        u, v = evaluate(f, 1.2)
+        @test u ≈ 0.946806012846268289646
+        @test v ≈ 0.155337561057228069719
 
-t = range(0.0, step=10.0, length=n)
-s1 = sin.(t * 2)
-s2 = s2 = 1.0 .- 2.0 * Bool[isodd(floor(Int, x / 3)) for x in t]
-s3 = Float64[mod(x, 5.0) for x in t]
+        f = icagfun(:tanh, 1.5f0)
+        u, v = evaluate(f, 1.2f0)
+        @test u ≈ 0.94680610f0
+        @test v ≈ 0.15533754f0
 
-s1 += 0.1 * randn(n)
-s2 += 0.1 * randn(n)
-s3 += 0.1 * randn(n)
+        f = icagfun(:gaus)
+        u, v = evaluate(f, 1.5)
+        @test u ≈ 0.486978701037524594696
+        @test v ≈ -0.405815584197937162246
 
-S = hcat(s1, s2, s3)'
-@assert size(S) == (k, n)
+        f = icagfun(:gaus, Float32)
+        u, v = evaluate(f, 1.5f0)
+        @test u ≈ 0.4869787f0
+        @test v ≈ -0.40581557f0
 
-A = randn(m, k)
+    end
 
-X = A * S
-mv = vec(mean(X, dims=2))
-@assert size(X) == (m, n)
-C = cov(X, dims=2)
+    ## data
+    @testset "Algorithm" begin
 
-# FastICA
+        # sources
+        n = 1000
+        k = 3
+        m = 8
+        X, μ, C = generatetestdata(n, k, m)
 
-M = fit(ICA, X, k; do_whiten=false, tol=Inf)
-@test isa(M, ICA)
-@test indim(M) == m
-@test outdim(M) == k
-@test mean(M) == mv
-W = M.W
-@test transform(M, X) ≈ W' * (X .- mv)
-@test W'W ≈ Matrix(I, k, k)
+        # FastICA
 
-M = fit(ICA, X, k; do_whiten=true, tol=Inf)
-@test isa(M, ICA)
-@test indim(M) == m
-@test outdim(M) == k
-@test mean(M) == mv
-W = M.W
-@test W'C * W ≈ Matrix(I, k, k)
+        M = fit(ICA, X, k; do_whiten=false, tol=Inf)
+        @test isa(M, ICA)
+        @test indim(M) == m
+        @test outdim(M) == k
+        @test mean(M) == μ
+        W = M.W
+        @test transform(M, X) ≈ W' * (X .- μ)
+        @test W'W ≈ Matrix(I, k, k)
 
-@test_throws StatsBase.ConvergenceException fit(ICA, X, k; do_whiten=true, tol=1)
+        M = fit(ICA, X, k; do_whiten=true, tol=Inf)
+        @test isa(M, ICA)
+        @test indim(M) == m
+        @test outdim(M) == k
+        @test mean(M) == μ
+        W = M.W
+        @test W'C * W ≈ Matrix(I, k, k)
 
-# Use data of different type
+        @test_throws StatsBase.ConvergenceException fit(ICA, X, k; do_whiten=true, tol=0.01)
 
-XX = convert(Matrix{Float32}, X)
+        # Use data of different type
 
-M = fit(ICA, XX, k; do_whiten=true, tol=Inf)
-@test eltype(mean(M)) == Float32
-@test eltype(M.W) == Float32
+        XX = convert(Matrix{Float32}, X)
 
-M = fit(ICA, XX, k; do_whiten=false, tol=Inf)
-@test isa(M, ICA)
-@test eltype(mean(M)) == Float32
-@test eltype(M.W) == Float32
-W = M.W
-@test transform(M, X) ≈ W' * convert(Matrix{Float32}, (X .- mv))
-@test W'W ≈ Matrix{Float32}(I, k, k)
+        M = fit(ICA, XX, k; do_whiten=true, tol=Inf)
+        @test eltype(mean(M)) == Float32
+        @test eltype(M.W) == Float32
+
+        M = fit(ICA, XX, k; do_whiten=false, tol=Inf)
+        @test isa(M, ICA)
+        @test eltype(mean(M)) == Float32
+        @test eltype(M.W) == Float32
+        W = M.W
+        @test transform(M, X) ≈ W' * convert(Matrix{Float32}, (X .- μ))
+        @test W'W ≈ Matrix{Float32}(I, k, k)
+
+        # input as view
+        M = fit(ICA, view(XX, :, 1:400), k; do_whiten=true, tol=Inf)
+        @test eltype(mean(M)) == Float32
+        @test eltype(M.W) == Float32
+    end
+
+end
