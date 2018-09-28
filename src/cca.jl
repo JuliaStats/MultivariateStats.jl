a2078085f58cff306646204d25f43c86905623b8
wildart <wildart@gmail.com>
2018-09-27 20:14:45 -0400

    fixed warning call

--- src/cca.jl
+++ src/cca.jl
@@ -223,7 +223,7 @@ function fit(::Type{CCA}, X::AbstractMatrix{T}, Y::AbstractMatrix{T};
         throw(DimensionMismatch("X and Y should have the same number of columns."))
 
     (n >= dx && n >= dy) ||
-        warn("CCA would be numerically instable when n < dx or n < dy.")
+        @warn("CCA would be numerically instable when n < dx or n < dy.")
 
     xmv = preprocess_mean(X, xmean)
     ymv = preprocess_mean(Y, ymean)
