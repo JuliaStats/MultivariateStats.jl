2c614297917812325411f9a039cc1811d931169c
Art <wildart@users.noreply.github.com>
2018-10-02 12:05:28 -0400

    ICA: Fixed convergence criteria (closes #45)
    - Added testsets.
    - Removed `Printf` dependency.

--- src/ica.jl
+++ src/ica.jl
@@ -1,10 +1,8 @@
 # Independent Component Analysis
 
-import Printf
-
 #### FastICA type
 
-mutable struct ICA{T<:Real}
+struct ICA{T<:Real}
     mean::Vector{T}   # mean vector, of length m (or empty to indicate zero mean)
     W::Matrix{T}      # component coefficient matrix, of size (m, k)
 end
@@ -61,8 +59,7 @@ function fastica!(W::DenseMatrix{T},      # initialized component matrix, size (
                   X::DenseMatrix{T},      # (whitened) observation sample matrix, size(m, n)
                   fun::ICAGDeriv{T},      # approximate neg-entropy functor
                   maxiter::Int,           # maximum number of iterations
-                  tol::Real,              # convergence tolerance
-                  verbose::Bool) where T<:Real          # whether to show iterative info
+                  tol::Real) where T<:Real# convergence tolerance
 
     # argument checking
     m = size(W, 1)
@@ -71,10 +68,7 @@ function fastica!(W::DenseMatrix{T},      # initialized component matrix, size (
     n = size(X, 2)
     k <= min(m, n) || throw(DimensionMismatch("k must not exceed min(m, n)."))
 
-    if verbose
-        Printf.@printf("FastICA Algorithm (m = %d, n = %d, k = %d)\n", m, n, k)
-        println("============================================")
-    end
+    @debug "FastICA Algorithm" m=m n=n k=k
 
     # pre-allocated storage
     Wp = Matrix{T}(undef, m, k)    # to store previous version of W
@@ -126,24 +120,11 @@ function fastica!(W::DenseMatrix{T},      # initialized component matrix, size (
         # symmetric decorrelation: W <- W * (W'W)^{-1/2}
         copyto!(W, W * _invsqrtm!(W'W))
 
-        # compare with Wp
-        chg = 0.0
-        for j = 1:k
-            s = 0.0
-            w = view(W,:,j)
-            wp = view(Wp,:,j)
-            for i = 1:m
-                s += abs(w[i] - wp[i])
-            end
-            if s > chg
-                chg = s
-            end
-        end
+        # compare with Wp to evaluate a conversion change
+        chg = maximum(abs.(abs.(diag(W*Wp')) .- 1.0))
         converged = (chg < tol)
 
-        if verbose
-            Printf.@printf("Iter %4d:  change = %.6e\n", t, chg)
-        end
+        @debug "Iteration $t" change=chg tolerance=tol
     end
     converged || throw(ConvergenceException(maxiter, chg, oftype(chg, tol)))
     return W
@@ -151,7 +132,7 @@ end
 
 #### interface function
 
-function fit(::Type{ICA}, X::DenseMatrix{T},                # sample matrix, size (m, n)
+function fit(::Type{ICA}, X::AbstractMatrix{T},                # sample matrix, size (m, n)
                           k::Int;                           # number of independent components
                           alg::Symbol=:fastica,             # choice of algorithm
                           fun::ICAGDeriv=icagfun(:tanh, T), # approx neg-entropy functor
@@ -159,8 +140,7 @@ function fit(::Type{ICA}, X::DenseMatrix{T},                # sample matrix, siz
                           maxiter::Integer=100,             # maximum number of iterations
                           tol::Real=1.0e-6,                 # convergence tolerance
                           mean=nothing,                     # pre-computed mean
-                          winit::Matrix{T}=zeros(T,0,0),    # init guess of W, size (m, k)
-                          verbose::Bool=false) where T<:Real             # whether to display iterations
+                          winit::Matrix{T}=zeros(T,0,0)) where T<:Real  # init guess of W, size (m, k)
 
     # check input arguments
     m, n = size(X)
@@ -175,7 +155,7 @@ function fit(::Type{ICA}, X::DenseMatrix{T},                # sample matrix, siz
     mv = preprocess_mean(X, mean)
     Z::Matrix{T} = centralize(X, mv)
 
-    W0= zeros(T, 0,0)    # whitening matrix
+    W0= zeros(T, 0, 0)  # whitening matrix
     if do_whiten
         C = rmul!(Z * transpose(Z), 1.0 / (n - 1))
         Efac = eigen(C)
@@ -189,7 +169,7 @@ function fit(::Type{ICA}, X::DenseMatrix{T},                # sample matrix, siz
     W = (isempty(winit) ? randn(T, size(Z,1), k) : copy(winit))
 
     # invoke core algorithm
-    fastica!(W, Z, fun, maxiter, tol, verbose)
+    fastica!(W, Z, fun, maxiter, tol)
 
     # construct model
     if do_whiten
