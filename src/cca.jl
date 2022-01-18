27219e1911bac525641bf3fecfb30134a565ebd0
Art Wild <wildart@gmail.com>
2022-01-18 10:03:46 -0500

    added docs to internal functions

--- src/cca.jl
+++ src/cca.jl
@@ -77,8 +77,22 @@ end
 
 #### Perform CCA on data
 
-## ccacov
+"""
+    ccacov(Cxx, Cyy, Cxy, xmean, ymean, p)
 
+Compute CCA based on analysis of the given covariance matrices, using generalized
+eigenvalue decomposition, and return [`CCA`](@ref) model.
+
+Parameters:
+- `Cxx`: The covariance matrix of `X`.
+- `Cyy`: The covariance matrix of `Y`.
+- `Cxy`: The covariance matrix between `X` and `Y`.
+- `xmean`: The mean vector of the **original** samples of `X`, which can be
+a vector of length `dx`, or an empty vector indicating a zero mean.
+- `ymean`: The mean vector of the **original** samples of `Y`, which can be
+a vector of length `dy`, or an empty vector indicating a zero mean.
+- `p`: The output dimension, *i.e* the dimension of the common space.
+"""
 function ccacov(Cxx::DenseMatrix{T},
                 Cyy::DenseMatrix{T},
                 Cxy::DenseMatrix{T},
@@ -141,8 +155,21 @@ function _ccacov(Cxx, Cyy, Cxy, xmean, ymean, p::Int)
     CCA(xmean, ymean, Px, Py, crs)
 end
 
-## ccasvd
 
+"""
+    ccasvd(Zx, Zy, xmean, ymean, p)
+
+Compute CCA based on singular value decomposition of centralized sample matrices `Zx` and `Zy`, and return [`CCA`](@ref) model[^1].
+
+Parameters:
+- `Zx`: The centralized sample matrix for `X`.
+- `Zy`: The centralized sample matrix for `Y`.
+- `xmean`: The mean vector of the **original** samples of `X`, which can be
+a vector of length `dx`, or an empty vector indicating a zero mean.
+- `ymean`: The mean vector of the **original** samples of `Y`, which can be
+a vector of length `dy`, or an empty vector indicating a zero mean.
+- `p`: The output dimension, *i.e* the dimension of the common space.
+"""
 function ccasvd(Zx::DenseMatrix{T},
                 Zy::DenseMatrix{T},
                 xmean::Vector{T},
